35c30bafeae090407dab0ecaf01df9054d1974ea
Dhanajigeobull <dhanajibhalerao.geobull@gmail.com>
2026-06-25 11:25:46 +0530

    Update responsive UI

--- src/containers/Spotlight.js
+++ src/containers/Spotlight.js
@@ -44,35 +44,50 @@ const Spotlights = ({
   const handleProjectClick = (projectId) => {
     history.push(`/projectdetail/${encodeURIComponent(projectId)}`);
   };
-
   const settings = {
     infinite: true,
+    speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
     arrows: false,
-    cssEase: "linear",
-    centerMode: false,
-    centerPadding: "0px",
+    dots: false,
+    adaptiveHeight: false,
+
     responsive: [
+      {
+        breakpoint: 1536,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
+          slidesToShow: 2,
         },
       },
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
         },
       },
     ],
   };
-
   const formatPrice = (price) => {
     if (!price) return "";
     price = parseInt(price);
@@ -190,18 +205,18 @@ const Spotlights = ({
   };
 
   return (
-    <div className="py-4 ml-10 mr-10 bg-white rounded-2xl">
-      <div className="flex flex-col items-center justify-between px-2 sm:flex-row">
+    <div className="px-3 py-4 bg-white rounded-2xl sm:px-4 md:px-6 lg:px-10">
+      <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
         <div className="flex flex-col text-center sm:text-left">
-          <h2 className="text-2xl font-bold text-gray-800 sm:text-3xl">
+          <h2 className="text-2xl font-bold text-gray-800 sm:text-3xl md:text-4xl">
             Builders Projects
           </h2>
           <p className="text-gray-500">Go from browsing to buying</p>
         </div>
 
-        <div className="flex items-center w-full mt-4 mb-3 space-x-3 sm:mt-0 sm:w-auto">
+        <div className="flex items-center justify-between w-full gap-2 md:w-auto">
           <button
-            className="w-full px-4 py-2 text-sm text-rose-700 bg-white my-border rounded-lg sm:px-6 sm:hidden"
+            className="px-4 py-2 text-xs bg-white rounded-lg sm:text-sm md:text-base my-border my-text whitespace-nowrap"
             onClick={handleClick}
           >
             View All Projects
@@ -216,7 +231,7 @@ const Spotlights = ({
             className="p-2 text-lg font-semibold text-gray-700 bg-white rounded-full shadow-md sm:text-2xl hover:shadow-lg"
             onClick={() => sliderRef.current.slickPrev()}
           >
-            <GoArrowLeft className="text-3xl text-black" />
+            <GoArrowLeft className="text-xl text-black md:text-2xl" />
           </button>
           <button
             className="p-2 text-lg font-semibold text-gray-700 bg-white rounded-full shadow-md sm:text-2xl hover:shadow-lg"
@@ -229,13 +244,13 @@ const Spotlights = ({
 
       <Slider ref={sliderRef} {...settings} className="mx-auto">
         {projectList.map((project, index) => (
-          <div key={index} className="px-4">
-            <div className="max-w-[500px] w-full bg-white rounded-3xl">
+          <div key={index} className="px-2 md:px-3">
+            <div className="w-full overflow-hidden bg-white shadow-lg rounded-3xl hover:shadow-xl transition-all duration-300">
               <div className="relative">
                 <img
                   src={project.cover_image}
                   alt={project.name}
-                  className="w-full h-[320px] object-cover rounded-t-3xl cursor-pointer"
+                  className="w-full h-[220px] sm:h-[260px] md:h-[300px] lg:h-[320px] object-cover rounded-t-3xl cursor-pointer"
                   onClick={() => handleProjectClick(project._id)}
                 />
                 <div className="absolute top-2 right-2 flex items-center space-x-2">
@@ -257,7 +272,7 @@ const Spotlights = ({
                     }}
                   >
                     <Heart
-                      size={22}
+                      size={20}
                       stroke={project.is_favorite ? "none" : "white"}
                       color={
                         project.is_favorite ? "red" : "rgba(75, 85, 99, 0.4) "
@@ -279,8 +294,8 @@ const Spotlights = ({
                   />
                 </div>
 
-                <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 w-[80%] sm:w-80 h-[100px] bg-gray-800/60 backdrop-blur-md flex flex-col justify-end p-4 rounded-t-3xl">
-                  <div className="absolute -top-7 left-1/2 transform -translate-x-1/2 bg-white border-2 border-gray-200 w-[55px] h-[55px] rounded-full flex justify-center items-center shadow-lg overflow-hidden">
+                <div className="absolute left-1/2 -translate-x-1/2 bottom-0 w-[90%] sm:w-[85%] md:w-[80%] h-[90px] md:h-[100px] bg-gray-800/60 backdrop-blur-md flex flex-col justify-end p-4 rounded-t-3xl">
+                  <div className="absolute -top-7 left-1/2 transform -translate-x-1/2 bg-white border-2 border-gray-200w-[50px] h-[50px] md:w-[60px] md:h-[60px] rounded-full flex justify-center items-center shadow-lg overflow-hidden">
                     <img
                       src={project.logo}
                       alt="Project Logo"
@@ -288,29 +303,29 @@ const Spotlights = ({
                     />
                   </div>
 
-                  <h3 className="text-center text-white text-lg font-semibold min-h-[24px]">
+                  <h3 className="text-center text-white text-base md:text-lg font-semibold line-clamp-2 min-h-[48px]">
                     {project.project_name || "No Project Name Available"}
                   </h3>
                 </div>
               </div>
 
               <div className="px-0">
-                <div className="p-3 bg-white shadow-sm rounded-b-3xl">
-                  <p className="text-lg font-semibold text-gray-800 p-0 m-1 truncate whitespace-nowrap overflow-hidden">
+                <div className="p-4 bg-white rounded-b-3xl">
+                  <p className="text-base md:text-lg font-semibold text-gray-800 line-clamp-2">
                     {project.congfigurations
                       ? project.congfigurations.includes("BHK")
                         ? project.congfigurations
                         : project.congfigurations
-                            .split(",")
-                            .map((c) => `${c.trim()} BHK`)
-                            .join(", ")
+                          .split(",")
+                          .map((c) => `${c.trim()} BHK`)
+                          .join(", ")
                       : "No Configurations"}
                   </p>
 
-                  <p className="text-xs uppercase tracking-wider text-gray-400 p-0 m-1">
+                  <p className="text-xs uppercase tracking-wider text-gray-400 mt-2">
                     {project.project_type}
                   </p>
-                  <p className="text-sm font-semibold text-slate-800 mt-2 flex items-center gap-2 truncate">
+                  <p className="text-sm md:text-base font-semibold text-slate-800 mt-2 flex items-center gap-2 line-clamp-1">
                     {/* <img
                       src="/image/address_icon.png"
                       alt="Location Icon"
@@ -346,7 +361,7 @@ const Spotlights = ({
                   </div> */}
                   <div className="flex-1 mt-3">
                     {project.average_project_price && (
-                      <h4 className="text-lg font-bold text-slate-800 mt-2">
+                      <h4 className="text-xl md:text-2xl font-bold text-slate-800 mt-3">
                         {" "}
                         {formatAverageProjectPrice(
                           Number(project.average_project_price),
@@ -358,8 +373,9 @@ const Spotlights = ({
               </div>
             </div>
           </div>
-        ))}
-      </Slider>
+        ))
+        }
+      </Slider >
       <div
         id="shareModal"
         className="fixed bottom-0 right-0 items-center justify-center bg-black bg-opacity-50 z-50 hidden"
@@ -438,7 +454,7 @@ const Spotlights = ({
           setIsLoginModalOpen(true);
         }}
       />
-    </div>
+    </div >
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
-import "./App.css";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "slick-carousel/slick/slick.css"; 
+import "./App.css";
+
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<GoogleOAuthProvider clientId="867447693808-ch4k6ili6o5lfjm2j0r96ka4imljaek4.apps.googleusercontent.com">
+			<App />
+		</GoogleOAuthProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
